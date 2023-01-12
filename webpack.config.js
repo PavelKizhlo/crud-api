f413211483283526d6e8a24db97fa364ebe65c4a
Pavel Kizhlo <pavelkizh@gmail.com>
2023-01-12 21:14:06 +0300

    feat: configure project environment

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const config = {
+  mode: 'production',
+  entry: './src/index.ts',
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+};
+
+export default config;
