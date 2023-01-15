44eeac45606da1321ed437990f2a759d69900b0b
Pavel Kizhlo <pavelkizh@gmail.com>
2023-01-15 21:48:43 +0300

    fix: configure start scripts to work with esm

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,38 @@
-import * as path from 'path';
-import { fileURLToPath } from 'url';
+import * as path from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const config = {
-  mode: 'production',
-  entry: './src/index.ts',
-  target: 'node',
+  mode: "production",
+  entry: "./src/index.ts",
+  target: "node18.12",
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.ts'],
+    extensions: [".js", ".ts"],
+    extensionAlias: {
+      ".js": [".ts", ".js"],
+    },
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
     clean: true,
+    library: {
+      type: "module",
+    },
+  },
+  experiments: {
+    outputModule: true,
   },
 };
 
