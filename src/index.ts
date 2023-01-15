bd7bbd657ccee8459adddf2ec4e8d902a6712ec9
Pavel Kizhlo <pavelkizh@gmail.com>
2023-01-15 21:53:45 +0300

    feat: add base routing

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,28 @@ import * as http from "http";
 
 dotenv.config();
 
-const PORT = +(<string>process.env["PORT"]);
+const PORT = +(<string>process.env["PORT"]) || 3000;
 const HOSTNAME = <string>process.env["HOSTNAME"];
 
-const server = http.createServer((_req, _res) => {
-  console.log("request");
+const server = http.createServer((req, res) => {
+  const route = req.url;
+  const { method } = req;
+
+  if (route === "/api/users" && method === "GET") {
+    console.log("get all");
+  } else if (route === "/api/users" && method === "POST") {
+    console.log("create new");
+  } else if (route?.match(/\/api\/users\/[0-9]+/) && method === "GET") {
+    console.log("get one");
+  } else if (route?.match(/\/api\/users\/[0-9]+/) && method === "PUT") {
+    console.log("update one");
+  } else if (route?.match(/\/api\/users\/[0-9]+/) && method === "DELETE") {
+    console.log("delete one");
+  } else {
+    console.log("404");
+  }
+
+  res.end();
 });
 
 server.listen(PORT, "localhost", () => {
